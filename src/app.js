1a9bcb630188bd528de01f0e0fa616ebd5a6ec1e
Talent <talentpthomas@gmail.com>
2024-08-25 12:05:51 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -15,35 +15,60 @@ let year = document.querySelector('#year');
 let runtime = document.querySelector('#runtime');
 let director = document.querySelector('#director');
 let lng = document.querySelector('#lng');
-let imbd = document.querySelector('#imbd');
+let imbd = document.querySelector('#imdb');
 let awards = document.querySelector('#awards');
 let poster = document.querySelector('#poster');
 let writers = document.querySelector('#writers')
-let container = document.querySelector('#container')
-
-
+let container = document.querySelector('#container');
+let error = document.querySelector('#error');
+let actors = document.querySelector('#actors');
+let suggestion = document.querySelector('#suggestion');
 function searchMovie(){
     let movieInput = document.querySelector("#movieName");
-    let query = api + movieInput.value
+    let query = api + movieInput.value;
 
     // async code fetch returns a promise
     fetch(query).then(data => data.json()).then(data => {
-        container.classList.remove('hidden');
-        container.classList.add('flex');
-        title.innerText = data.Title;
-        plot.innerText  = data.Plot;
-        genre.innerText = data.Genre;
-        director.innerText = data.Director;
-        lng.innerText = data.Language;
-        awards.innerText = data.Awards;
-        year.innerText = data.Year;
-        runtime.innerText = data.Runtime;
-        writers.innerText = data.Writer;
-        poster.src = data.Poster;
 
+        if (data.Error == 'Movie not found!'){
+            container.classList.add('hidden');
+            error.classList.remove('hidden');
+        }
+        else{
+            if (data.imdbRating >= 8){
+                suggestion.innerText = 'Must Watch';
+                suggestion.style.backgroundColor = '#2ECC71';
+                suggestion.style.color = '#FFFFFF';
+            }else if (data.imdbRating >= 6){
+                suggestion.innerText = 'Woth Watching';
+                suggestion.style.backgroundColor = '#4169E1';
+                suggestion.style.color = '#FFFFFF';
+            } else if (data.imdbRating >= 4) {
+                suggestion.innerText = 'Average';
+                suggestion.style.backgroundColor = '#D3D3D3';
+                suggestion.style.color = '#000000';
+            } else {
+                suggestion.innerText = 'Time Waste'
+                suggestion.style.backgroundColor = '#DC143C';
+                suggestion.style.color = '#FFFFFF'; 
+            } 
+            error.classList.add('hidden');
+            container.classList.remove('hidden');
+            container.classList.add('flex');
+            actors.innerText = data.Actors;
+            title.innerText = data.Title;
+            plot.innerText  = data.Plot;
+            genre.innerText = data.Genre;
+            director.innerText = data.Director;
+            lng.innerText = data.Language;
+            imbd.innerText  = data.imdbRating;
+            awards.innerText = data.Awards;
+            year.innerText = data.Year;
+            runtime.innerText = data.Runtime;
+            writers.innerText = data.Writer;
+            poster.src = data.Poster;
+        }
     })
-
-    
 }
 
 btn.addEventListener('mouseover', ()=>{
